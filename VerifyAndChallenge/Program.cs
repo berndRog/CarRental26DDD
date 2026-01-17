b9c4ad887e7504810a19b44a80b30071f23da6b9
Bernd-Uwe Rogalla <b-u.rogalla@ostfalia.de>
2026-01-17 16:44:24 +0100

    Add initial project files for verifier and challenge; implement SHA256 hashing and base64 encoding for secure data handling

---
+++ VerifyAndChallenge/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+var bytes = RandomNumberGenerator.GetBytes(64);
+var verifier = Convert.ToBase64String(bytes)
+   .Replace("+","-").Replace("/","_").Replace("=","");
+
+var hash = SHA256.HashData(Encoding.ASCII.GetBytes(verifier));
+var challenge = Convert.ToBase64String(hash)
+   .Replace("+","-").Replace("/","_").Replace("=","");
+
+Console.WriteLine("Verifier");
+Console.WriteLine(verifier);
+Console.WriteLine("Challenge:");
+Console.WriteLine(challenge);
